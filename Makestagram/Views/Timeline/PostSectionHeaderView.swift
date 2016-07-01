38951b6eff0ef97e7846945edb846b9619c7f3f3
moonhues <veron@vt.sg>
2016-07-01 23:43:12 +0800

    Completed Makestagram 2 API installation

--- Makestagram/Views/Timeline/PostSectionHeaderView.swift
+++ Makestagram/Views/Timeline/PostSectionHeaderView.swift
@@ -18,6 +18,11 @@ class PostSectionHeaderView: UITableViewCell {
         didSet {
             if let post = post {
                 usernameLabel.text = post.user?.username
+                // 1
+                postTimeLabel.text = post.createdAt?.shortTimeAgoSinceDate(NSDate()) ?? ""
+                
+/* We are reading the createdAt date from the post. This is a property that Parse sets automatically on all PFObjects. Then we use an extension provided by the DateTools library: shortTimeAgoSinceDate(_:). This method takes a comparison date. By calling NSDate() we create a date object with the current time. If the post has been created 4 hours ago, this line of code will generate the string "4h". Since createdAt? is an optional, we use the ?? operator to fall back to an empty string, just in case the createdAt date is nil.
+ */
             }
         }
     }
